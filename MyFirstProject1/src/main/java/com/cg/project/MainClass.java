564c0e8aa62d58fba833e4e3ed9fa040b7cc907a
lr5 <lr5@DIN78000428.corp.capgemini.com>
2019-07-04 12:48:10 +0530

    My First GIT Commit

---
+++ MyFirstProject1/src/main/java/com/cg/project/MainClass.java
@@ -0,0 +1,11 @@
+package com.cg.project;
+
+public class MainClass 
+{
+public static void main(String args[])
+{
+	Sales s= new Sales();
+	s.display();
+	
+}
+}
---
+++ MyFirstProject1/src/main/java/com/cg/project/Sales.java
@@ -0,0 +1,9 @@
+package com.cg.project;
+
+public class Sales 
+{
+public void display()
+{
+	System.out.println("Hai , Loki Loves You");
+}
+}
